3c17846ee8c9e9b0072b48007ef47f8c4b6e05fe
AdamRohdell <rohdell@student.chalmers.se>
2019-12-06 17:23:51 +0100

    Started on the tetris

--- Tetris.hs
+++ Tetris.hs
@@ -1,1 +1,23 @@
 import Data.List
+
+data Color = Red | Blue | Green | Yellow
+
+type Block = Maybe Color
+
+newtype Tetrimino = Tetrimino [Block]
+
+blocks:: Tetrimino -> [Block]
+blocks (Tetrimino bs) = bs
+
+type Row = [Block]
+
+emptyTetrimino = [n,n,n,n,n,n,n,n]
+        where
+            n = Nothing
+
+emptyRow = [n,n,n,n,n,n,n,n,n,n]
+        where
+            n = Nothing
+isValidTetrimino :: Tetrimino -> Bool
+isValidTetrimino t = length (blocks t) < 16 && isTetrimino t 
+
